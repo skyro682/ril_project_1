3e9d047bb7e0a5c563ce96b685cdae6bef26714c
Richard <williot.richard@gmail.com>
2020-12-03 12:20:07 +0100

    interface
    interface

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,56 @@
 @extends('layout.app')
 
 @section('body')
-    
-    <h1>hello world</h1>
+<br>
+<!-- body Section 1-->
 
+
+    <section class="page-section bg-primary text-white" id="1">
+        <div class="container">
+            <!-- section 1 Section Heading-->
+            <h2 class="page-section-heading text-center text-uppercase text-white">titre musique</h2>
+            <h5 class=" text-center text-uppercase text-white">auteur</h5>
+
+            <!-- section 1 Section Content-->
+            <div class="row">
+                <div class="col-lg-3"></div>
+                <div class="col-lg-6 text-center">
+                    <p class="lead">Contenu.</p>
+                </div>
+            </div>
+            <!-- spotify Section-->
+            <div class="text-center mt-4">
+                Spotify
+            </div>
+            <!-- more Section-->
+            <div class="text-center mt-4">
+                <button type="button" class="btn btn-info">Voir plus...</button>
+            </div>
+        </div>
+    </section>
+
+<!-- body Section 2-->
+<section class="page-section" id="2">
+    <div class="container">
+        <!-- About Section Heading-->
+        <h2 class="page-section-heading text-center text-uppercase">titre musique</h2>
+        <h5 class=" text-center text-uppercase">auteur</h5>
+        <!-- About Section Content-->
+        <div class="row">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-8 text-center">
+                <p class="lead">Contenu.</p>
+            </div>
+        </div>
+        <!-- spotify Section-->
+        <div class="text-center mt-4">
+            Spotify
+        </div>
+        <!-- more Section-->
+        <div class="text-center mt-4">
+            <button type="button" class="btn btn-info">Voir plus...</button>
+        </div>
+    </div>
+</section>
+<br>
 @endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,19 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ril_project_1</title>
 
     <!-- Script -->
-
+    <link rel="stylesheet" href="../public/js/scripts.js">
     <!-- Style -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="../public/css/styles.css">
 
 </head>
+
 <body class="">
+    <!-- Navigation-->
+    <nav class="navbar bg-secondary text-uppercase fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">Musica</a>
+            <button type="button" class="btn btn-info">Se connecter</button>
+        </div>
+    </nav>
+
     @section('body')
-        
+
     @show
+
+    <!-- Copyright Section-->
+    <div class="copyright py-4 text-center text-white fixed-bottom">
+        <div class="container"><small>Copyright © Musica 2020</small></div>
+    </div>
 </body>
+
 </html>
